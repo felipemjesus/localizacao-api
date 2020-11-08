a6a23a1eef3d750c2318d92caa944dccdc473144
Felipe Martins <felipemjesus@outlook.com>
2020-11-08 18:46:20 -0300

    Criado as migrations e ajuste no docker-compose

---
+++ database/migrations/2020_11_08_205720_create_locations_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLocationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('locations', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->unsignedBigInteger('location_type_id');
+            $table->unsignedBigInteger('parent_location_id');
+            $table->timestamps();
+
+            $table->foreign('location_type_id')
+                ->references('id')
+                ->on('location_types');
+
+            $table->foreign('parent_location_id')
+                ->references('id')
+                ->on('locations')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('locations');
+    }
+}
---
+++ database/migrations/2020_11_08_210240_create_addresses_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAddressesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('addresses', function (Blueprint $table) {
+            $table->id();
+            $table->integer('user_id');
+            $table->string('formatted_address');
+            $table->string('latitude');
+            $table->string('longitude');
+            $table->unsignedBigInteger('location_id');
+            $table->timestamps();
+
+            $table->foreign('location_id')
+                ->references('id')
+                ->on('locations')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('addresses');
+    }
+}
